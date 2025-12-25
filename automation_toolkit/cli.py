b620e2b8220a234063c5bc41ee983d2a3dc8b308
Balakrishna Arigala <balakrishna.arigala26@gmail.com>
2025-12-25 23:29:35 +0530

    style: format cli with black

--- automation_toolkit/cli.py
+++ automation_toolkit/cli.py
@@ -3,14 +3,12 @@ import click
 
 @click.command()
 @click.option(
-    "--interval",
-    default=10,
-    show_default=True,
-    help="Monitor interval in seconds"
+    "--interval", default=10, show_default=True, help="Monitor interval in seconds"
 )
 def main(interval):
     """Python Automation Toolkit CLI"""
     click.echo(f"Monitoring every {interval} seconds")
+
 
 if __name__ == "__main__":
     main()
